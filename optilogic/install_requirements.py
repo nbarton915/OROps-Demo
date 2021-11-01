dbe41af82a1a1985a27aaced01136d2dcc97e7e9
Nathan <nbarton915@gmail.com>
2021-11-01 16:21:21 -0600

    fixed pathing

--- optilogic/install_requirements.py
+++ optilogic/install_requirements.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 def ensure_requirements():
-    result = subprocess.run(f'pip3 install -r {os.getcwd()}/requirements.txt', capture_output=True, shell=True)
+    result = subprocess.run(f'pip3 install -r "{os.getcwd()}/requirements.txt"', capture_output=True, shell=True)
     result.check_returncode()
     print(result.stdout.decode('utf-8'))
